5ccdde3b4dc7bf5339c28c627aff01745d55e553
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-16 14:18:10 +1200

    added home page

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 import sqlite3
 
 
-# im a pp poo poo :)
+# im a pp poo poo :) - ,matthew
 app = Flask(__name__)
+
 
 @app.route("/")
 def homely_home():
-    return render_template("Index.html")
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    # i am turning red from turning red the movie
