eba407d2ee8bcfe5697ef774730a5fe0ae239a25
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-20 12:41:24 +1200

    started all page

--- app.py
+++ app.py
@@ -2,13 +2,37 @@ from flask import Flask, render_template
 import sqlite3
 
 
-# im a pp poo poo :) - ,matthew
 app = Flask(__name__)
 
 
 @app.route("/")
-def homely_home():
+def home():
     return render_template("home.html")
+
+
+@app.route("/all")
+def all():
+    return render_template("all.html")
+
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+
+@app.route("/sub")
+def sub():
+    return render_template("sub.html")
+
+
+@app.route("/special")
+def special():
+    return render_template("special.html")
+
+
+@app.route("/type")
+def type():
+    return render_template("type.html")
 
 
 if __name__ == "__main__":
