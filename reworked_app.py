fc734a60f3eff816d25872cddbf85ffdcb3ff15d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-04 15:20:01 +1200

    added some stuff

--- reworked_app.py
+++ reworked_app.py
@@ -13,7 +13,8 @@ app.config['SECRET_KEY'] = "MyReallySecretKey"
 
 PAGESIZE = 12
 
-def connect_database(query, id = None):
+
+def connect_database(query, id=None):
     db = sqlite3.connect(DB)
     cursor = db.cursor()
     if id:
@@ -25,19 +26,33 @@ def connect_database(query, id = None):
     db.close()
     return results
 
+
 def get_page(table, pagenum):
-    offset = (pagenum  - 1) * PAGESIZE
-    weapon = connect_database("SELECT * FROM ? LIMIT ? OFFSET ?", (table, PAGESIZE, offset))
-    return weapon
+    offset = (pagenum - 1) * PAGESIZE
+    query = "SELECT * FROM %.13s LIMIT ? OFFSET ?;" % (table)
+    print(query)
+    weapon = connect_database(query, (PAGESIZE, offset))
+    return weapon[0]
+
+
+def select_weapon(query, weaponid):
+    weapon = connect_database(query, weaponid)
+    if weapon[0] is not None:
+        return weapon[0]
+    else:
+        return None
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/main/<int:page>/<int:weaponID>")
 def main(page, weaponID):
-    weapon = get_page("main", page)
-    return render_template("main.html", weapon=weapon)
+    weapons = get_page("MainWeapon", page)
+    selected_weapon = select_weapon("SELECT * FROM MainWeapons WHERE MainWeaponID = ?;", (weaponID, ))
+    return render_template("main.html", weapons=weapons, selected_weapon=selected_weapon)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
