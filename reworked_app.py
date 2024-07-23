ea9784c18133d3a3c16fce3264c4cdcb663e99ae
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-23 14:40:43 +1200

    fixed images and image name and remade login/admin

--- reworked_app.py
+++ reworked_app.py
@@ -7,7 +7,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 DB = "splatoon3.db"
-LOGINDB = "login.db"
 
 app.config['SECRET_KEY'] = "MyReallySecretKey"
 
@@ -122,6 +121,59 @@ def all(page, weaponID):
         selected_weapon = (0, 0)
         columns = (0, 0)
     return render_template("all.html", page=page, total_pages=total_pages, weapons=weapons, selected_weapon=selected_weapon, columns=columns, title="all")
+
+
+@app.route("/admin_login", methods=["GET", "POST"])
+def admin_login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        query = "SELECT password FROM Users WHERE username = ?"
+        correct_password = connect_database(query, (username,))[0][0]
+        if check_password_hash(correct_password, password):
+            print("password correct")
+            return render_template("admin.html")
+        else:
+            print("password incorrect")
+    return render_template("admin_login.html")
+
+
+@app.post("/add_weapon")
+def add_weapon():
+    print(request.form)
+    update = request.form["update"]
+    table = request.form["table"]
+    id = request.form["id"]
+    weapon_name = request.form["weapon_name"]
+    main_weapon = request.form["main_weapon"]
+    main_weapon_id = connect_database(f"SELECT MainWeaponID FROM MainWeapon WHERE MainWeaponName = '{main_weapon}'")[0][0]
+    sub_weapon = request.form["sub_weapon"]
+    sub_weapon_id = connect_database(f"SELECT SubWeaponID FROM SubWeapon WHERE SubWeaponName = '{sub_weapon}'")[0][0]
+    special_weapon = request.form["special_weapon"]
+    special_weapon_id = connect_database(f"SELECT SpecialWeaponID FROM SpecialWeapon WHERE SpecialWeaponName = '{special_weapon}'")[0][0]
+    points = request.form["points"]
+    version = request.form["version"]
+    if update == "update":
+        query = f"UPDATE Weapons SET WeaponName = '{weapon_name}', MainWeaponID = '{main_weapon_id}', SubWeaponID = '{sub_weapon_id}', SpecialWeaponID = '{special_weapon_id}', SpecialPoint = '{points}', VersionID = '{version}' WHERE WeaponID = {id}"
+    else:
+        query = f"INSERT INTO Weapons (WeaponName, MainWeaponID, SubWeaponID, SpecialWeaponID, SpecialPoint, VersionID) VALUES ('{weapon_name}',{main_weapon_id},{sub_weapon_id},{special_weapon_id},{points},{version})"
+    connect_database(query)
+    return redirect("/")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        hashed_password = generate_password_hash(password)
+        query = "INSERT INTO Users (Username, Password) VALUES (?,?)"
+        connect_database(query, (username, hashed_password))
+    return render_template("signup.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("/404.html", error=e), 404
 
 
 if __name__ == "__main__":
