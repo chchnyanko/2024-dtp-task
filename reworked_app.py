1661a3eb5f801e62c568801aa2c410ef6fcac628
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-09 10:26:15 +1200

    finished the selected weapon data for other pages

--- reworked_app.py
+++ reworked_app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+'''Splatoon 3 wiki'''
 import sqlite3
 from math import ceil
+from flask import Flask, render_template, request, redirect, flash
+
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -14,6 +16,7 @@ PAGESIZE = 12
 
 
 def connect_database(query, id=None):
+    '''Execute queries and commit changes to the database'''
     db = sqlite3.connect(DB)
     cursor = db.cursor()
     if id:
@@ -26,23 +29,16 @@ def connect_database(query, id=None):
     return results
 
 
-def get_columns(query):
-    db = sqlite3.connect(DB)
-    cursor = db.cursor()
-    cursor.execute(query)
-    results = [description[0] for description in cursor.description]
-    db.close()
-    return results
-
-
 def get_page(table, offset):
-    query = "SELECT * FROM %.13s LIMIT ? OFFSET ?;" % (table)
+    '''Get the max number of pages'''
+    query = f"SELECT * FROM {table} LIMIT ? OFFSET ?;"
     weapon = connect_database(query, (PAGESIZE, offset))
     return weapon
 
 
-def select_weapon(query, weaponid):
-    weapon = connect_database(query, weaponid)
+def select_weapon(query, weapon_id):
+    '''Get the data for the selected weapon'''
+    weapon = connect_database(query, weapon_id)
     try:
         return weapon[0]
     except:
@@ -51,81 +47,84 @@ def select_weapon(query, weaponid):
 
 @app.route("/")
 def home():
+    '''The homepage'''
     return render_template("home.html")
 
 
-@app.route("/main/<int:page>/<int:weaponID>")
-def main(page, weaponID):
+@app.route("/main/<int:page>/<int:weapon_id>")
+def main(page, weapon_id):
+    '''The page for main weapons'''
     total_pages = ceil(connect_database("SELECT COUNT (*) FROM MainWeapon")[0][0] / 12)
     if page <= total_pages:
         offset = (page - 1) * PAGESIZE
     else:
         offset = (total_pages - 1) * PAGESIZE
-    weapons = get_page("MainWeapon", offset)
-    if weaponID != 0:
-        selected_weapon = select_weapon("SELECT * FROM MainWeapon WHERE MainWeaponID = ?;", (weaponID, ))
+    main_weapons = get_page("MainWeapon", offset)
+    if weapon_id != 0:
+        selected_weapon = select_weapon("SELECT * FROM MainWeapon WHERE Mainweapon_id = ?;", (weapon_id, ))
         weapon_type = connect_database("SELECT WeaponType FROM WeaponTypes WHERE WeaponTypeID = ?;", (selected_weapon[2],))
     else:
         selected_weapon = (0, 0)
-    return render_template("reworked_main.html", page=page, total_pages=total_pages, weapons=weapons, selected_weapon=selected_weapon, weapon_type=weapon_type, title="main")
+        weapon_type = 0
+    return render_template("reworked_main.html", page=page, total_pages=total_pages, weapons=main_weapons, selected_weapon=selected_weapon, weapon_type=weapon_type, title="main")
 
 
-@app.route("/sub/<int:page>/<int:weaponID>")
-def sub(page, weaponID):
+@app.route("/sub/<int:page>/<int:weapon_id>")
+def sub(page, weapon_id):
+    '''The page for sub weapons'''
     total_pages = ceil(connect_database("SELECT COUNT (*) FROM SubWeapon")[0][0] / 12)
     if page <= total_pages:
         offset = (page - 1) * PAGESIZE
     else:
         offset = (total_pages - 1) * PAGESIZE
-    weapons = get_page("SubWeapon", offset)
-    if weaponID != 0:
-        selected_weapon = select_weapon("SELECT * FROM SubWeapon WHERE SubWeaponID = ?;", (weaponID, ))
-        
+    sub_weapons = get_page("SubWeapon", offset)
+    if weapon_id != 0:
+        selected_weapon = select_weapon("SELECT * FROM SubWeapon WHERE Subweapon_id = ?;", (weapon_id, ))
     else:
         selected_weapon = (0, 0)
-    return render_template("reworked_main.html", page=page, total_pages=total_pages, weapons=weapons, selected_weapon=selected_weapon, title="sub")
+    return render_template("reworked_main.html", page=page, total_pages=total_pages, weapons=sub_weapons, selected_weapon=selected_weapon, title="sub")
 
 
-@app.route("/special/<int:page>/<int:weaponID>")
-def special(page, weaponID):
+@app.route("/special/<int:page>/<int:weapon_id>")
+def special(page, weapon_id):
+    '''The page for special weapons'''
     total_pages = ceil(connect_database("SELECT COUNT (*) FROM SpecialWeapon")[0][0] / 12)
     if page <= total_pages:
         offset = (page - 1) * PAGESIZE
     else:
         offset = (total_pages - 1) * PAGESIZE
-    weapons = get_page("SpecialWeapon", offset)
-    if weaponID != 0:
-        selected_weapon = select_weapon("SELECT * FROM SpecialWeapon WHERE SpecialWeaponID = ?;", (weaponID, ))
-        
+    special_weapons = get_page("SpecialWeapon", offset)
+    if weapon_id != 0:
+        selected_weapon = select_weapon("SELECT * FROM SpecialWeapon WHERE Specialweapon_id = ?;", (weapon_id, ))
     else:
         selected_weapon = (0, 0)
-    return render_template("reworked_main.html", page=page, total_pages=total_pages, weapons=weapons, selected_weapon=selected_weapon, title="special")
+    return render_template("reworked_main.html", page=page, total_pages=total_pages, weapons=special_weapons, selected_weapon=selected_weapon, title="special")
 
 
-@app.route("/all/<int:page>/<int:weaponID>")
-def all(page, weaponID):
+@app.route("/all/<int:page>/<int:weapon_id>")
+def weapons(page, weapon_id):
+    '''The page for all the weapons and their kits'''
     total_pages = ceil(connect_database("SELECT COUNT (*) FROM Weapons")[0][0] / 12)
     if page <= total_pages:
         offset = (page - 1) * PAGESIZE
     else:
         offset = (total_pages - 1) * PAGESIZE
-    weapons = get_page("Weapons", offset)
-    if weaponID != 0:
-        selected_weapon = select_weapon("SELECT * FROM Weapons WHERE WeaponID = ?", (weaponID, ))
-        columns = get_columns("SELECT * FROM Weapons")
+    all_weapons = get_page("Weapons", offset)
+    if weapon_id != 0:
+        selected_weapon = select_weapon("SELECT * FROM Weapons WHERE weapon_id = ?", (weapon_id, ))
         print("hello", selected_weapon)
         if len(selected_weapon) > 2:
             selected_weapon += (int(ceil(int(selected_weapon[2])/12)),)
-            selected_weapon += (connect_database("SELECT SubWeaponName FROM SubWeapon WHERE SubWeaponID = ?", (selected_weapon[3], ))[0][0],)
-            selected_weapon += (connect_database("SELECT SpecialWeaponName FROM SpecialWeapon WHERE SpecialWeaponID = ?", (selected_weapon[4], ))[0][0],)
+            selected_weapon += (connect_database("SELECT SubWeaponName FROM SubWeapon WHERE Subweapon_id = ?", (selected_weapon[3], ))[0][0],)
+            selected_weapon += (connect_database("SELECT SpecialWeaponName FROM SpecialWeapon WHERE Specialweapon_id = ?", (selected_weapon[4], ))[0][0],)
     else:
         selected_weapon = (0, 0)
-        columns = (0, 0)
-    return render_template("all.html", page=page, total_pages=total_pages, weapons=weapons, selected_weapon=selected_weapon, columns=columns, title="all")
+    return render_template("all.html", page=page, total_pages=total_pages, weapons=all_weapons, selected_weapon=selected_weapon, title="all")
 
 
 @app.route("/admin_login", methods=["GET", "POST"])
 def admin_login():
+    '''The admin page for the admin login'''
     try:
         if request.method == "POST":
             username = request.form["username"]
@@ -149,6 +148,7 @@ def admin_login():
 
 @app.post("/add_weapon")
 def add_weapon():
+    '''The admin page for adding and editing data from the database'''
     print(request.form)
     update = request.form["update"]
     table = request.form["table"]
@@ -157,28 +157,29 @@ def add_weapon():
     weapon_name = request.form["weapon_name"]
     main_weapon = request.form["main_weapon"]
 
-    main_weapon_id = connect_database("SELECT MainWeaponID FROM MainWeapon WHERE MainWeaponName = ?", (main_weapon,))[0][0]
+    main_weapon_id = connect_database("SELECT Mainweapon_id FROM MainWeapon WHERE MainWeaponName = ?", (main_weapon,))[0][0]
     sub_weapon = request.form["sub_weapon"]
-    sub_weapon_id = connect_database("SELECT SubWeaponID FROM SubWeapon WHERE SubWeaponName = ?", (sub_weapon,))[0][0]
+    sub_weapon_id = connect_database("SELECT Subweapon_id FROM SubWeapon WHERE SubWeaponName = ?", (sub_weapon,))[0][0]
     special_weapon = request.form["special_weapon"]
-    special_weapon_id = connect_database("SELECT SpecialWeaponID FROM SpecialWeapon WHERE SpecialWeaponName = ?", (special_weapon, ))[0][0]
+    special_weapon_id = connect_database("SELECT Specialweapon_id FROM SpecialWeapon WHERE SpecialWeaponName = ?", (special_weapon, ))[0][0]
     
     points = request.form["points"]
     version = request.form["version"]
     if update == "update":
-        query = "UPDATE Weapons SET WeaponName = ?, MainWeaponID = ?, SubWeaponID = ?, SpecialWeaponID = ?, SpecialPoint = ?, VersionID = ? WHERE WeaponID = ?"
+        query = "UPDATE Weapons SET WeaponName = ?, Mainweapon_id = ?, Subweapon_id = ?, Specialweapon_id = ?, SpecialPoint = ?, VersionID = ? WHERE weapon_id = ?"
         connect_database(query, (weapon_name, main_weapon_id, sub_weapon_id, special_weapon_id, points, version, id))
     elif update == "add":
-        query = "INSERT INTO Weapons (WeaponName, MainWeaponID, SubWeaponID, SpecialWeaponID, SpecialPoint, VersionID) VALUES (?,?,?,?,?,?)"
+        query = "INSERT INTO Weapons (WeaponName, Mainweapon_id, Subweapon_id, Specialweapon_id, SpecialPoint, VersionID) VALUES (?,?,?,?,?,?)"
         connect_database(query, (weapon_name, main_weapon_id, sub_weapon_id, special_weapon_id, points, version))
     elif update == "delete":
-        query = "DELETE FROM Weapons WHERE WeaponID = ?"
+        query = "DELETE FROM Weapons WHERE weapon_id = ?"
         connect_database(query, (id, ))
     return redirect("/")
 
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    '''The admin page for adding admin users'''
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -190,6 +191,7 @@ def signup():
 
 @app.errorhandler(404)
 def page_not_found(e):
+    '''The 404 error page'''
     return render_template("/404.html", error=e), 404
 
 
