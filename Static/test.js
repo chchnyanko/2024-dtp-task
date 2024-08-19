328e7c7b85418986cbccabc2d298b67a918d4551
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-19 13:01:26 +1200

    did js

--- Static/test.js
+++ Static/test.js
@@ -1,14 +1,33 @@
+let update = document.getElementById("update")
+let table = document.getElementById("table");
 let path = window.location.pathname;
 let fileName = path.substring(path.lastIndexOf('/') + 1);
 if(fileName == "admin_login"){
-    document.getElementById("update").addEventListener("change", change_update)
-    document.getElementById("table").addEventListener("change", change_table)
+    document.getElementById("update").addEventListener("change", change_update);
+    document.getElementById("table").addEventListener("change", change_table);
 }
 function change_update() {
-    let update = document.getElementById("update")
-    console.log(update.value)
+    update = document.getElementById("update");
+    console.log(update.value);
 }
 function change_table() {
-    let update = document.getElementById("table")
-    console.log(update.value)
+    table = document.getElementById("table");
+    console.log(table.value);
+    update_labels()
+}
+
+const label_names = {
+    weapon:["Weapon ID", "Weapon Name", "Main Weapon", "Sub Weapon", "Special Weapon", "Points for Special", "Introduced Version"],
+    main:["Weapon ID", "Weapon Name", "Weapon Type", "Damage", "Range", "Attack Rate", "Ink Usage", "Speed While Shooting"],
+    sub:["Weapon ID", "Weapon Name", "Damage", "Ink Consumption", "Tracking Duration", "Damage Duration"],
+    special:["Weapon ID", "Weapon Name", "Damage", "Number of Attacks", "Duration"]
+}
+function update_labels() {
+    let hi = document.getElementsByClassName("admin-label");
+    for(let i = 0; i < hi.length; i++){
+        console.log(i);
+        console.log(hi[i])
+        console.log(table.value)
+        hi[i].innerHTML = label_names[table.value][i]
+    }
 }
